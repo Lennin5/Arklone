f95e08c2728e129718148087776f910f43876b9b
Lennin5 <lenninlemus0@gmail.com>
2024-07-16 11:25:09 -0600

    feat(GameManager): agregado de método RestartScene al activarse el LoseTrigger, y llamado desde otro script

--- Scripts/BallCollision.cs
+++ Scripts/BallCollision.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class BallCollision : MonoBehaviour
 {
 
+    GameManager gameManager; // Variable para almacenar el script GameManager
+
+    private void Start()
+    {
+        gameManager = FindObjectOfType<GameManager>(); // Buscamos el script GameManager en la escena (lo hace de manera automática, no es necesaria ninguna referencia)
+    }
+
     // Función que se ejecuta cuando la pelota colisiona con otro objeto (cualquiera), la variable collision contiene la información del objeto con el que colisionó
     private void OnCollisionEnter2D(Collision2D collision)
     {
         //Debug.Log("Collision detected, details: " + collision.gameObject.name);
-
-
-        if (collision.gameObject.tag == "Block") // podemos usar también collision.gameObject.CompareTag("Block")
+        if (collision.gameObject.tag == "Block") // Podemos usar también if(collision.gameObject.CompareTag("Block"))
             Destroy(collision.gameObject); // Destruimos el objeto con el que colisionó la pelota
+    }
+
+    // Función que se ejecuta cuando la pelota entra en contacto con un trigger (un objeto con el componente Collider2D con la propiedad IsTrigger activada)
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        //Debug.Log("Se disparó el trigger, detalles: " + collider.gameObject.name);
+        gameManager.RestartScene(); // Llamamos a la función RestartScene del script GameManager
     }
 }
---
+++ Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Función que se ejecuta cuando se presiona el botón de reinicio
+    public void RestartScene()
+    {
+        // Recargamos la escena actual
+        SceneManager.LoadScene("SampleScene");
+    }
+}
